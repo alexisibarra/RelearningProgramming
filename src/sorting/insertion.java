5fe57fbde3c846c5e80e230ca4cf0fd823fdc294
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-01-28 12:21:23 -0430

    Cambios para implementar con arraylist

--- src/sorting/insertion.java
+++ src/sorting/insertion.java
@@ -1,16 +1,26 @@
 package sorting;
 
+import java.util.List;
+
+import static sorting.utils.isGreaterThan;
+import static sorting.utils.swap;
+
 /**
  * Created by alexis on 23/01/17.
  */
-public class insertion extends sorter{
-    public static void sort(Comparable[] a) {
-        int N = a.length;
+public class insertion extends sorter {
+    public insertion(List<Comparable> aToSort) {
+        setArray(aToSort);
+        sort(array);
+    }
+
+    public void sort(List<Comparable> a) {
+        int N = a.size();
 
         for (int i=1; i < N; i++){
             int j = i;
 
-            while(j > 0 && isGreaterThan(a[j-1], a[j])){
+            while(j > 0 && isGreaterThan(a.get(j - 1), a.get(j))){
                 swap(a, j-1, j);
                 j--;
             }
--- src/sorting/selection.java
+++ src/sorting/selection.java
@@ -1,17 +1,27 @@
 package sorting;
 
+import java.util.List;
+
+import static sorting.utils.isLessThan;
+import static sorting.utils.swap;
+
 /**
  * Created by alexis on 23/01/17.
  */
-public class selection extends sorter{
-    public static void sort(Comparable[] a) {
-        int N = a.length;
+public class selection extends sorter {
+    public selection(List<Comparable> aToSort) {
+        setArray(aToSort);
+        sort(array);
+    }
+
+    public void sort(List<Comparable> a) {
+        int N = a.size();
 
         for (int i=0; i < N; i++){
             int min = i;
 
             for (int j=i+1; j < N; j++)
-                if(isLessThan(a[j], a[min]))
+                if(isLessThan(a.get(j), a.get(min)))
                     min = j;
 
             swap(a, i, min);
