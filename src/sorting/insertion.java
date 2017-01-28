a0a0b2a10e11672d7a6e3e709afbfe24b3828eb2
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-01-28 12:33:56 -0430

    Refactorizacieon de los bucles del algoritmo

--- src/sorting/insertion.java
+++ src/sorting/insertion.java
@@ -17,13 +17,8 @@ public class insertion extends sorter {
     public void sort(List<Comparable> a) {
         int N = a.size();
 
-        for (int i=1; i < N; i++){
-            int j = i;
-
-            while(j > 0 && isGreaterThan(a.get(j - 1), a.get(j))){
+        for (int i=1; i < N; i++)
+            for (int j = i; j > 0 && isGreaterThan(a.get(j - 1), a.get(j)) ; j--)
                 swap(a, j-1, j);
-                j--;
-            }
-        }
     }
 }
