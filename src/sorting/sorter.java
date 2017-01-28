a4d4a9cd25aa052995f06f977f6db115c46e1234
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-01-28 11:33:21 -0430

    Renombrando funciones

--- src/sorting/sorter.java
+++ src/sorting/sorter.java
@@ -4,22 +4,23 @@ package sorting;
  * Created by alexis on 23/01/17.
  */
 public class sorter {
-    protected static boolean less(Comparable v, Comparable w){
+    protected static boolean isLessThan(Comparable v, Comparable w){
         return v.compareTo(w) < 0;
     }
-    protected static void exch(Comparable[] a, int i, int j) {
+    protected static boolean isGreaterThan(Comparable v, Comparable w){
+        return !isLessThan(v,w);
+    }
+    protected static void swap(Comparable[] a, int i, int j) {
         Comparable t = a[i]; a[i] = a[j]; a[j] = t;
     }
     public static void show(Comparable[] a) {
-     // Print the array, on a single line.
-        for (int i = 0; i < a.length; i++)
-            System.out.print(a[i] + " ");
+        for (Comparable anA : a) System.out.print(anA + " ");
         System.out.println();
     }
-    public static boolean isSorted(Comparable[] a)
-    { // Test whether the array entries are in order.
+    public static boolean isSorted(Comparable[] a) {
         for (int i = 1; i < a.length; i++)
-            if (less(a[i], a[i-1])) return false;
+            if (isLessThan(a[i], a[i - 1]))
+                return false;
         return true;
     }
 }
