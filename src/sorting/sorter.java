ae047420eeb46770f92569111cccfe889ae1c257
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2017-01-28 12:18:05 -0430

    Renombrando desde sorter.java empleando notarion de arraylists

--- src/sorting/sorter.java
+++ src/sorting/sorter.java
@@ -1,26 +1,21 @@
 package sorting;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Created by alexis on 23/01/17.
+ * Created by alexis on 28/01/17.
  */
-public class sorter {
-    protected static boolean isLessThan(Comparable v, Comparable w){
-        return v.compareTo(w) < 0;
+public abstract class sorter {
+    List<Comparable> array = new ArrayList<>();
+
+    public List<Comparable> getArray() {
+        return array;
     }
-    protected static boolean isGreaterThan(Comparable v, Comparable w){
-        return !isLessThan(v,w);
+
+    public void setArray(List<Comparable> array) {
+        this.array = array;
     }
-    protected static void swap(Comparable[] a, int i, int j) {
-        Comparable t = a[i]; a[i] = a[j]; a[j] = t;
-    }
-    public static void show(Comparable[] a) {
-        for (Comparable anA : a) System.out.print(anA + " ");
-        System.out.println();
-    }
-    public static boolean isSorted(Comparable[] a) {
-        for (int i = 1; i < a.length; i++)
-            if (isLessThan(a[i], a[i - 1]))
-                return false;
-        return true;
-    }
+
+    abstract void sort(List<Comparable> a);
 }
